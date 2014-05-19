2e108cd411de735f6ce36838fb12245489f5afaa
mejas <lego_boy11@yahoo.com>
2014-05-19 13:43:44 +0800

    TDD implementation of area

---
+++ src/Cards.Extensions.Tfs.Tests/Area.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cards.Extensions.Tfs.Tests
+{
+    public class Area
+    {
+
+        protected static List<Area> Storage;
+        public static void Reset()
+        {
+            Storage = new List<Area>();
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Area Add(string areaName)
+        {
+            var area = new Area()
+            {
+                Name = areaName,
+                ID = 1,
+                CreatedDate = new DateTime(2014, 5, 19),
+                ModifiedDate = new DateTime(2014, 5, 19)
+            };
+
+            //add storage fugg
+            Storage.Add(area);
+
+            return area;
+        }
+
+        public List<Area> GetAll()
+        {
+            return Storage;
+        }
+
+        public Area Get(int id)
+        {
+            return Storage.Find(item => item.ID == id);
+        }
+    }
+}
---
+++ src/Cards.Extensions.Tfs.Tests/AreaTests.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace Cards.Extensions.Tfs.Tests
+{
+    public class AreaTests
+    {
+        public class Structure
+        {
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenInitialize_ShouldNotBeNull()
+            {
+                var subject = new Area();
+                subject.Should().NotBeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenInitialize_ShouldIDBeZero()
+            {
+                var subject = new Area();
+                subject.ID.Should().Be(0);
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenInitialize_ShouldNameBeNull()
+            {
+                var subject = new Area();
+                subject.Name.Should().BeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenInitialize_ShouldCreatedDateBeMinValue()
+            {
+                var subject = new Area();
+                subject.CreatedDate.Should().Be(DateTime.MinValue);
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenInitialize_ShouldModifiedDateBeMinValue()
+            {
+                var subject = new Area();
+                subject.ModifiedDate.Should().Be(DateTime.MinValue);
+            }
+        }
+
+        public class AddMethod
+        {
+            public AddMethod()
+            {
+                Area.Reset();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenNameIsNotNull_ShouldNotBeNull()
+            {
+                //Arrange
+                var area= new Area();
+                var areaName = "Backlog";
+
+                //Act
+                var subject = area.Add(areaName);
+
+                //Assert
+                subject.Should().NotBeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenNameIsNotNull_NameShouldBeBacklog()
+            {
+                var area = new Area();
+                var areaName = "Backlog";
+
+                var subject = area.Add(areaName);
+
+                subject.Name.Should().Be("Backlog");
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenNameIsNotNull_IDShouldBeOne()
+            {
+                var area = new Area();
+                var areaName = "Backlog";
+
+                var subject = area.Add(areaName);
+
+                subject.ID.Should().Be(1);
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenNameIsNotNull_CreatedDateShouldHaveValue()
+            {
+                var area = new Area();
+                
+                var areaName = "Backlog";
+
+                var subject = area.Add(areaName);
+
+                subject.CreatedDate.Should().Be(new DateTime(2014, 5, 19));
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenNameIsNotNull_ModifiedDateShouldHaveValue()
+            {
+                var area = new Area();
+                var areaName = "Backlog";
+
+                var subject = area.Add(areaName);
+
+                subject.ModifiedDate.Should().Be(new DateTime(2014, 5, 19));
+            }
+        }
+
+        public class GetAllMethod
+        {
+            public GetAllMethod()
+            {
+                Area.Reset();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGetAll_ShouldNotBeNull()
+            {
+                var area = new Area();
+                var subject = area.GetAll();
+
+                subject.Should().NotBeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGetAll_ShouldBeEmpty()
+            {
+                var area = new Area();
+                var subject = area.GetAll();
+
+                subject.Should().BeEmpty();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGetAll_AndWithNewArea_ShouldHaveOneElement()
+            {
+                var area = new Area();
+
+                area.Add("Backlog");
+
+                var subject = area.GetAll();
+
+                subject.Count.Should().Be(1);
+            }
+        }
+
+
+        public class GetMethod
+        {
+            public GetMethod()
+            {
+                Area.Reset();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGet_ShouldBeNull()
+            {
+                var area = new Area();
+                var id = 1;
+
+                var subject = area.Get(id);
+
+                subject.Should().BeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGet_AndWithNewArea_ShouldNotBeNull()
+            {
+                var area = new Area();
+
+                area.Add("Backlog");
+
+                var subject = area.Get(1);
+
+                subject.Should().NotBeNull();
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGet_AndWithNewArea_IDShouldBeOne()
+            {
+                var area = new Area();
+
+                area.Add("Backlog");
+
+                var subject = area.Get(1);
+
+                subject.ID.Should().Be(1);
+            }
+
+            [Fact]
+            [Trait("Category", "Area")]
+            public void WhenGet_AndWithNewArea_NameShouldBeBacklog()
+            {
+                var area = new Area();
+
+                area.Add("Backlog");
+
+                var subject = area.Get(1);
+
+                subject.Name.Should().Be("Backlog");
+            }
+        }
+    }
+}
