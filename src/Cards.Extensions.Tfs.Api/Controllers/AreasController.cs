7757f62672af40d2e69daf83ff039e7ad0f8c8eb
mejas <lego_boy11@yahoo.com>
2014-05-20 22:28:43 +0800

    EF database initial implementation

--- src/Cards.Extensions.Tfs.Api/Controllers/AreasController.cs
+++ src/Cards.Extensions.Tfs.Api/Controllers/AreasController.cs
@@ -3,29 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
 namespace Cards.Extensions.Tfs.Api.Controllers
 {
-
     [Authorize]
     public class AreasController : ApiController
     {
-
         public List<Area> GetAllAreas()
         {
-            var identity = HttpContext.Current.Request.LogonUserIdentity;
+            var area = new Area();
 
-            return new List<Area>()
-            {
-                new Area() { ID = 1, Name = "Backlog", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue, CreatedUser = identity.Name, ModifiedUser = identity.Name },
-                new Area() { ID = 2, Name = "Todo", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue, CreatedUser = identity.Name, ModifiedUser = identity.Name },
-                new Area() { ID = 3, Name = "Doing", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue, CreatedUser = identity.Name, ModifiedUser = identity.Name },
-                new Area() { ID = 4, Name = "Done", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue, CreatedUser = identity.Name, ModifiedUser = identity.Name }
-            };
-
+            return area.GetAll();
         }
 
         public Area GetAreaByID(int id)
@@ -33,6 +23,11 @@ namespace Cards.Extensions.Tfs.Api.Controllers
             var identity = HttpContext.Current.Request.LogonUserIdentity;
 
             return new Area() { ID = id, Name = "Backlog", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue, CreatedUser = identity.Name, ModifiedUser = identity.Name };
+        }
+
+        public Area AddArea(string name)
+        {
+            throw new NotImplementedException();
         }
 
         public Area UpdateArea(Area area)
