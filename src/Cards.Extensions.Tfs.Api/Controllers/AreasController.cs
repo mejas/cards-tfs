b3ab4e78f66c1ac56945c678f6a589d8adf5480b
Marc Vitalis <marc.vitalis@live.com>
2014-05-20 10:34:27 +0800

    add api structure and setup

---
+++ src/Cards.Extensions.Tfs.Api/Controllers/AreasController.cs
@@ -0,0 +1,29 @@
+﻿using Cards.Extensions.Tfs.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cards.Extensions.Tfs.Api.Controllers
+{
+
+    [Authorize]
+    public class AreasController : ApiController
+    {
+
+        public List<Area> GetAllAreas()
+        {
+            return new List<Area>()
+            {
+                new Area() { ID = 1, Name = "Backlog", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue },
+                new Area() { ID = 2, Name = "Todo", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue },
+                new Area() { ID = 3, Name = "Doing", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue },
+                new Area() { ID = 4, Name = "Done", CreatedDate = DateTime.MinValue, ModifiedDate = DateTime.MinValue }
+            };
+
+        }
+
+    }
+}
