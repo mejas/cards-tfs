56afa5140def34875da3e4d69649ee4547e71f46
mejas <lego_boy11@yahoo.com>
2014-05-20 13:12:33 +0800

    added Identity Provider

---
+++ src/Cards.Extensions.Tfs.Core/IdentityProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cards.Extensions.Tfs.Core
+{
+    public class IdentityProvider : IIdentityProvider
+    {
+        public string UserName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
