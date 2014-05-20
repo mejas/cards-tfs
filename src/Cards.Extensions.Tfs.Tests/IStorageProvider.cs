c1730e67586d085250f9341c939d70a909b0496d
mejas <lego_boy11@yahoo.com>
2014-05-20 11:04:07 +0800

    decoupled storage module

---
+++ src/Cards.Extensions.Tfs.Tests/IStorageProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cards.Extensions.Tfs.Tests
+{
+    public interface IStorageProvider
+    {
+        List<Area> GetAllAreas();
+        Area GetArea(int id);
+
+        Area Add(Area area);
+        Area Update(Area area);
+    }
+}
